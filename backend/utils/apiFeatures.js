a10edd800beb40fa781e9380fe2a8bcd7f7762cd
PASHOKGITHUB <pashokbabu.38@gmail.com>
2024-07-05 12:21:46 -1200

    first commit to branch Ashok

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -18,16 +18,20 @@ class APIFeatures {
     return this;
   }
 
-  filter(){
-    const queryStrCopy={...this.querystr}
+  filter() {
+    const queryStrCopy = { ...this.querystr };
 
     //removing fields from query
-    const removeFields=['keyword','limit','page']
-    removeFields.forEach(field=>delete queryStrCopy[field])
+    const removeFields = ["keyword", "limit", "page"];
+    removeFields.forEach(field => delete queryStrCopy[field]);
 
-    this.query.find(queryStrCopy)
-    return this
+    let querystr = JSON.stringify(queryStrCopy);
+
+    querystr=querystr.replace(/\b(gt|gte|lt|lte)/g, match => `$${match}`);
+
+    this.query.find(JSON.parse(querystr));
+    return this;
   }
 }
 
-module.exports=APIFeatures
+module.exports = APIFeatures;
